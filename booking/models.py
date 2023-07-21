5c828d27e5067fb73f5de03e099da53d7cf860fd
Driveronhire <martech@driveronhire.com>
2023-07-21 19:04:42 +0530

    working on placebooking

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from client_management.models import AddClient
 from driver_management.models import AddDriver
+from datetime import datetime, date
+from django.utils import timezone 
+from dateutil.parser import parse
 
 
 
@@ -15,10 +18,19 @@ class Clientregistration(models.Model):
     def __str__(self):
         return self.full_name
     
-    
+packege=(
+        ('4Hours', '4Hours'),
+        ('8Hours', '8Hours'),
+        ('Night', 'Night')
+
+    )
+  
 class PlaceBooking(models.Model):
     client_name = models.ForeignKey(Clientregistration, on_delete=models.CASCADE)
-    currunt_loaction=models.CharField(max_length=200, null=True, blank=True)
+    trip_type=models.CharField(max_length=50, null=True ,blank=True)
+    packege=models.CharField(choices=packege, max_length=20, default='4Hours')
+    user_curr_lat=models.FloatField()
+    user_curr_long=models.FloatField()
     from_date = models.DateField()
     to_date = models.DateField()
     car_type=models.CharField(max_length=100, null=True)
@@ -26,8 +38,12 @@ class PlaceBooking(models.Model):
     pickup_location=models.CharField(max_length=100, null=True)
     drop_location=models.CharField(max_length=100, null=True)
     driver=models.ForeignKey(AddDriver, on_delete=models.CASCADE)
+    booking_time=models.DateTimeField(auto_now_add=True)
+   
+
     def __str__(self):
         return self.client_name.full_name
+    
 
     # no_of_days = models.PositiveIntegerField()
     # start_time = models.TimeField()
--- booking/serializers.py
+++ booking/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import PlaceBooking, Clientregistration
+from .models import  *
 from driver_management.models import AddDriver
 
 
