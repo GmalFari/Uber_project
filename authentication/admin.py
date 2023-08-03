7e3b0dfb97de202e5d979ad042b92e2b03a679a9
Driveronhire <martech@driveronhire.com>
2023-08-03 11:40:47 +0530

    authyentications

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from .models import NewUser
+from .models import Newuser
 # # Register your models here.
 
-# admin.site.register(NewUser)
+admin.site.register(Newuser)
--- authentication/models.py
+++ authentication/models.py
@@ -1,49 +1,50 @@
-# from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# from django.db import models
-# from django.utils import timezone
+from django.db import models
+from django.utils import timezone
 
 
-# class CustomAccountManager(BaseUserManager):
-#     def create_user(self, email, user_name, first_name, password, **other_fields):
-#         if not email:
-#             raise ValueError(('You must provide an email address'))
-#         # convert all in lowercase
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, user_name=user_name,
-#                           first_name=first_name, **other_fields)
-#         user.set_password(password)
-#         user.save()
-#         return user
+class CustomAccountManager(BaseUserManager):
+    def create_user(self, email, user_name, first_name, password, **other_fields):
+        if not email:
+            raise ValueError(('You must provide an email address'))
+        # convert all in lowercase
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name,
+                          first_name=first_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
-#     def create_superuser(self, email, user_name, first_name, password, **other_fields):
-#         other_fields.setdefault('is_staff', True)
-#         other_fields.setdefault('is_superuser', True)
-#         other_fields.setdefault('is_active', True)
+    def create_superuser(self, email, user_name, first_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', False)
+        other_fields.setdefault('is_user', True)
 
-#         if other_fields.get('is_staff') is not True:
-#             raise ValueError('Superuser must be assigned to is_staff=True.')
-#         if other_fields.get('is_superuser') is not True:
-#             raise ValueError(
-#                 'Superuser must be assigned to is_superuser=True.')
-#         return self.create_user(email, user_name, first_name, password, **other_fields)
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+        return self.create_user(email, user_name, first_name, password, **other_fields)
 
-# # 2. Creating Custom User Model
-# class NewUser(AbstractBaseUser, PermissionsMixin):
-#     phone = models.BigIntegerField(unique=True)
-#     user_name = models.CharField(max_length=150, )
-#     first_name = models.CharField(max_length=150)
-#     start_date = models.DateTimeField(default=timezone.now)
+# 2. Creating Custom User Model
+class Newuser(AbstractBaseUser, PermissionsMixin):
+    phone = models.BigIntegerField(unique=True)
+    user_name = models.CharField(max_length=150, )
+    first_name = models.CharField(max_length=150)
+    start_date = models.DateTimeField(default=timezone.now)
+    email= models.EmailField()
     
-#     is_staff = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_user = models.BooleanField(default=True)
 
-#     objects = CustomAccountManager()
+    objects = CustomAccountManager()
 
-#     USERNAME_FIELD = 'phone'
+    USERNAME_FIELD = 'phone'
 
-#     # required for superuser
-#     REQUIRED_FIELDS = ['user_name']
+    # required for superuser
+    REQUIRED_FIELDS = ['user_name','first_name','email']
 
-#     def __str__(self):
-#         return self.user_name
+    def __str__(self):
+        return str(self.phone)
